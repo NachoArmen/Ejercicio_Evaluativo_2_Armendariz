d8a0db70522473530aff72685b666b14b650a1b8
NachoArmen <ignacio.armendariz@davinci.edu.ar>
2023-10-17 09:36:19 -0300

    Commit inicial

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ from Cine import Cine
 
 #Cine 1
 c1 = Cine("Hoyts", "Unicenter 123")
+c2 = Cine("Cinemark", "Palermo 456")
 p1 = Pelicula_Animada("Toy Story", 90, "Animación", "Pixar")
 p2 = Pelicula("Die Hard", 120, "Accion")
 
 db = Conexion("mi_db.db")
 db.crear_tabla()
 db.agregar_pelicula(p1.titulo, p1.duracion, p1.genero)
+db.agregar_pelicula(p2.titulo, p2.duracion, p2.genero)
 c1.agregar_pelicula(p1)
+c1.agregar_pelicula(p2)
+c2.agregar_pelicula(p1)
 
 peliculas = db.lista_peliculas()
 print(peliculas)
 
 c1.mostrar_programacion()
+c2.mostrar_programacion()
